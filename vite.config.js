36b2d6c7e15209230ebbca336ed2f707b8379be9
alan-kalkan <alan0613@live.fr>
2024-09-08 12:53:59 +0200

    build bite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
   css: {
     postcss: {
@@ -11,6 +12,7 @@ export default defineConfig({
     },
   },
   build: {
+    outDir: "dist",
     assetsInlineLimit: 0,
   },
 });
